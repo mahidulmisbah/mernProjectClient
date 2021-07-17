6c7fee10c0f7143a892cffb90b52fe7632e6dad6
Kazi Md. Mahidul Islam <mahidul.cse.du@gmail.com>
2021-07-18 04:31:39 +0600

    connect client with server (redux react-redux redux-thunk)

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
-import React from "react";
+import React, {useEffect} from "react";
 import { Container,AppBar,Typography,Grow,Grid } from "@material-ui/core";
+import { useDispatch } from "react-redux";
+
+import { getPosts } from './actions/posts'
 import Form from "./component/form/form"
 import Posts from "./component/posts/posts"
 import useStyle from "./style"
@@ -8,6 +11,11 @@ import myfb from "./images/myfb.png"
 const App = ()=>
 {
     const classes = useStyle();
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(getPosts())
+    },[dispatch])
     return(
         <Container maxidth = "lg" >
             <AppBar className = {classes.appBar} position = "static" color="inherit">
@@ -16,7 +24,7 @@ const App = ()=>
             </AppBar>
             <Grow in>
                 <Container>
-                    <Grid container justify = "space-between" alignItems = "stretch" spacing = {3} >
+                    <Grid container justifyContent = "space-between" alignItems = "stretch" spacing = {3} >
                         <Grid item xs = {12} sm = {7} > <Posts/> </Grid>
                         <Grid item xs = {12} sm = {4} > <Form/> </Grid>
                     </Grid>
--- src/component/form/form.js
+++ src/component/form/form.js
@@ -1,6 +1,8 @@
 import React from "react";
+import useStyle from "./style"
 
 const Form =() => {
+    const classes = useStyle();
     return (
         <h1>Form</h1>
     )
