59f47a04abed4e480c31aec6a1dc3e6b12e5c2e4
Kazi Md. Mahidul Islam <mahidul.cse.du@gmail.com>
2021-07-17 23:56:33 +0600

    Project setup file and initial front end view

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Container,AppBar,Typography,Grow,Grid } from "@material-ui/core";
+import Form from "./component/form/form"
+import Posts from "./component/posts/posts"
+import useStyle from "./style"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
+import myfb from "./images/myfb.png"
+const App = ()=>
+{
+    const classes = useStyle();
+    return(
+        <Container maxidth = "lg" >
+            <AppBar className = {classes.appBar} position = "static" color="inherit">
+                <Typography className = {classes.heading} varient = "h2" align = "center">MY FB</Typography>
+                <img className = {classes.image} src={myfb} alt="myfb" height = "60"/>
+            </AppBar>
+            <Grow in>
+                <Container>
+                    <Grid container justify = "space-between" alignItems = "stretch" spacing = {3} >
+                        <Grid item xs = {12} sm = {7} > <Posts/> </Grid>
+                        <Grid item xs = {12} sm = {4} > <Form/> </Grid>
+                    </Grid>
+                </Container>
+            </Grow>
+        </Container>
+    )
+};
 export default App;
